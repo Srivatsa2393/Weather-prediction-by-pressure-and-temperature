902b356158eb1a306292a6a17ba075d299a5cf03
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-19 16:50:51 +0530

    Added the initial App component

--- src/components/app.js
+++ src/components/app.js
@@ -3,7 +3,34 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <div className="container">
+          <div className="row">
+            <h1 className="text-center">
+              Weather Predictor
+            </h1>
+          </div>
+
+          <div className="row">
+            <div className="col-md-6">
+              Slider
+            </div>
+            <div className="col-md-6">
+              Graph
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-md-6">
+              Slider
+            </div>
+            <div className="col-md-6">
+              Graph
+            </div>
+          </div>
+
+        </div>
+      </div>
     );
   }
 }
